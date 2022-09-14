def32c3f8d82a987a1b316822c5a098451b77a13
GooA-NA <nasirgudesev@gmail.com>
2022-09-14 23:14:41 +0300

    Корзина: добавление и удаление

--- routes/user.route.js
+++ routes/user.route.js
@@ -5,5 +5,9 @@ const { usersController } = require('../controllers/user.controller');
 router.get('/user', usersController.getAllUsers);
 router.post('/user', usersController.addUser);
 router.post('/login', usersController.login);
+router.patch('/basket', usersController.addInBasket)
+router.patch('/basket/remove', usersController.removeFromBasket)
+router.get('/basket/:userId', usersController.getBasket)
+
 
 module.exports = router;
