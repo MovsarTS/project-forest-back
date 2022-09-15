9760922f92d3ae40064d983e600802e2714ca738
GooA-NA <nasirgudesev@gmail.com>
2022-09-15 21:37:39 +0300

    Корзина в самом разгаре

--- routes/product.route.js
+++ routes/product.route.js
@@ -4,5 +4,7 @@ const { productController } = require('../controllers/product.controller');
 
 router.post('/product', productController.addProduct);
 router.get('/products', productController.getProduct);
+router.patch('/basket/plus', productController.countPlus)
+router.patch('/basket/minus', productController.countMinus)
 
 module.exports = router;
